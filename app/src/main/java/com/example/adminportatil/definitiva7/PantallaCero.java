dce535943c7c417fe6912f4b5385b1486c3a2c81
JonDePedro <jondepedro@gmail.com>
2017-12-27 12:12:52 +0100

    asdasdsad

---
+++ app/src/main/java/com/example/adminportatil/definitiva7/PantallaCero.java
@@ -0,0 +1,10 @@
+package com.example.adminportatil.definitiva7;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adminportatil on 27/12/2017.
+ */
+
+public class PantallaCero extends AppCompatActivity {
+}
