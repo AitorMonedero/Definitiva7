1ffb493699407460a9df9de378ec4a4e5ea9c9ff
JonDePedro <jondepedro@gmail.com>
2017-12-27 10:57:14 +0100

    La tercera pantala comienza a dar sus frutos

---
+++ app/src/main/java/com/example/adminportatil/definitiva7/TerceraPantalla.java
@@ -0,0 +1,20 @@
+package com.example.adminportatil.definitiva7;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adminportatil on 20/12/2017.
+ */
+
+public class TerceraPantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tercera_pantalla);
+
+
+    }
+}
