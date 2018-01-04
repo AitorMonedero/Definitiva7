98ecdb52b0221efbde5e3bd98d46c653fd0f9cf1
JonDePedro <jondepedro@gmail.com>
2018-01-04 12:51:52 +0100

    Botones para la tercera pantalla

--- app/src/main/java/com/example/adminportatil/definitiva7/TerceraPantalla.java
+++ app/src/main/java/com/example/adminportatil/definitiva7/TerceraPantalla.java
@@ -3,16 +3,27 @@ package com.example.adminportatil.definitiva7;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Aitor & Jon on 20/12/2017.
  */
 
 public class TerceraPantalla extends AppCompatActivity {
+
+
+    private Button plus,minus,volver,siguiente;
+    private int cantidad=0;
+    private String cantidadString;
+    private TextView cantidadTotal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +36,7 @@ public class TerceraPantalla extends AppCompatActivity {
         final String[] bebidas; // Array de los datos de los tamaños de pizza
 
         //Rellenamos el array DATOS.
-        bebidas = new String[]{"Pizza Barbacoa","Pizza Carbonara","Pizza 4 Quesos","Pizza Vegetal","Pizza Tropical"};
+        bebidas = new String[]{"Coca-Cola","Kas","Agua","Cerveza","Nestea","RedBull"};
 
         //Elemento ArrayAdapter, que permite coger un Array como fuente de información.
         ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource (this, R.array.SpinnerBebidas, android.R.layout.simple_spinner_item);
@@ -58,5 +69,59 @@ public class TerceraPantalla extends AppCompatActivity {
 
             }
         });
+
+
+        plus=(Button)findViewById(R.id.btnPlus);
+        minus=(Button)findViewById(R.id.btnMinus);
+        volver=(Button) findViewById(R.id.btnVolver);
+        siguiente=(Button) findViewById(R.id.btnSiguiente);
+        cantidadTotal=(TextView)findViewById(R.id.txtCantidad);
+
+        cantidadString=String.valueOf(cantidad);
+        cantidadTotal.setText(cantidadString);
+
+        plus.setOnClickListener(new View.OnClickListener() {
+                                    public void onClick(View arg0){
+                                        cantidad=cantidad+1;
+                                        cantidadString=String.valueOf(cantidad);
+                                        cantidadTotal.setText(cantidadString);
+                                    }
+                                }
+
+        );
+
+        minus.setOnClickListener(new View.OnClickListener() {
+                                     public void onClick(View arg0){
+                                         if(cantidad<1){
+                                             Toast toast1 = Toast.makeText(getApplicationContext(), "No puedes pedir menos de 0 bebidas.", Toast.LENGTH_SHORT);
+                                             toast1.show();
+                                         }else{
+                                             cantidad=cantidad-1;
+                                             cantidadString=String.valueOf(cantidad);
+                                             cantidadTotal.setText(cantidadString);
+                                         }
+                                     }
+                                 }
+
+        );
+
+        volver.setOnClickListener(new View.OnClickListener() {
+                                      public void onClick(View arg0){
+                                          Intent intentaa=new Intent(TerceraPantalla.this,SegundaPantalla.class);
+                                          startActivity(intentaa);
+                                      }
+                                  }
+
+        );
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+                                         public void onClick(View arg0){
+                                             Intent intentaar=new Intent(TerceraPantalla.this,TerceraPantalla.class);
+                                             startActivity(intentaar);
+                                         }
+                                     }
+
+        );
+
     }
 }
